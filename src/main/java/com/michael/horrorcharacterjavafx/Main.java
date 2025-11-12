8ea00f59e07d417126e76aafa2a85fd773357ecd
Maicheal Shenouda <rory1980rory1980@gmail.com>
2025-11-12 11:45:02 -0500

    working

--- src/main/java/com/michael/horrorcharacterjavafx/Main.java
+++ src/main/java/com/michael/horrorcharacterjavafx/Main.java
@@ -10,15 +10,15 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/michael/horrorcharacterjavafx/view/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public FXMLLoader loadFXML(String name) {
-        return new FXMLLoader(Main.class.getResource(name + ".fxml"));
+        return new FXMLLoader(getClass().getResource(name + ".fxml"));
     }
 
     public static void main(String[] args) {
