e716e0ebf558f89208ba2cc07f1f3e03591d99c3
Maicheal Shenouda <rory1980rory1980@gmail.com>
2025-12-01 12:19:54 -0500

    Created primary fxml scene. Need to add functionality. Program runs as of now still.

--- src/main/java/com/michael/horrorcharacterjavafx/Main.java
+++ src/main/java/com/michael/horrorcharacterjavafx/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/michael/horrorcharacterjavafx/view/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 700);
+        stage.setTitle("Horror Character Manager");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/michael/horrorcharacterjavafx/controller/PrimaryController.java
@@ -0,0 +1,26 @@
+package com.michael.horrorcharacterjavafx.controller;
+
+import com.michael.horrorcharacterjavafx.model.Zombie;
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class PrimaryController {
+
+    @FXML
+    private ListView<String> mainAnchorPane;
+
+   // @FXML
+   // private ObservableList<String> items = mainAnchorPane.getItems();
+
+
+
+
+
+
+
+
+    /// @FXML
+   // public void initialize() { addinitialItems(); }
+}
